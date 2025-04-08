943d26d6d8fb6e5c8b40d2a564228a693b63b645
Igor Panteleyev <panteleev.igor69@gmail.com>
2025-04-09 01:55:11 +0300

    update

--- pgadmin/rootfs/etc/cont-init.d/30-nginx.sh
+++ pgadmin/rootfs/etc/cont-init.d/30-nginx.sh
@@ -6,27 +6,10 @@ set -e
 # NGINX SETTING #
 #################
 
-declare pgadmin_protocol=http
-
 # Generate Ingress configuration
-if bashio::config.true 'ssl'; then
-    bashio::config.require.ssl
-    pgadmin_protocol=https
-    sed -i "s|9000|9443|g" /etc/nginx/includes/upstream.conf
-    sed -i "s|9000|9443|g" /etc/services.d/nginx/run
-    sed -i "s|9099 default_server|9099 ssl|g" /etc/nginx/templates/ingress.gtpl
-    sed -i '7 i ssl_certificate /ssl/{{ .certfile }};' /etc/nginx/templates/ingress.gtpl
-    sed -i '7 i ssl_certificate_key /ssl/{{ .keyfile }};' /etc/nginx/templates/ingress.gtpl
-    bashio::log.info "Ssl enabled, please use https for connection"
-fi
-
 bashio::var.json \
     interface "$(bashio::addon.ip_address)" \
-    port "^$(bashio::addon.ingress_port)" \
-    protocol "${pgadmin_protocol}" \
-    certfile "$(bashio::config 'certfile')" \
-    keyfile "$(bashio::config 'keyfile')" \
-    ssl "^$(bashio::config 'ssl')" |
+    port "^$(bashio::addon.ingress_port)" |
 tempio \
     -template /etc/nginx/templates/ingress.gtpl \
     -out /etc/nginx/servers/ingress.conf
--- pgadmin/rootfs/etc/nginx/templates/ingress.gtpl
+++ pgadmin/rootfs/etc/nginx/templates/ingress.gtpl
@@ -6,8 +6,9 @@ server {
   client_max_body_size 0;
 
   location / {
-    proxy_pass {{ .protocol }}://backend/;
+    proxy_pass http://backend/;
     resolver 127.0.0.11 valid=180s;
+    proxy_set_header X-Script-Name $http_x_ingress_path;
     proxy_set_header Connection "";
     proxy_connect_timeout 30m;
     proxy_send_timeout 30m;
