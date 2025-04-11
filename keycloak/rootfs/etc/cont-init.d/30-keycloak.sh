e45207f5c0c7c7618d844b6a3c8f44ff7418e118
Igor Panteleyev <panteleev.igor69@gmail.com>
2025-04-12 01:54:23 +0300

    Add keycloak

--- keycloak/rootfs/etc/cont-init.d/30-keycloak.sh
+++ keycloak/rootfs/etc/cont-init.d/30-keycloak.sh
@@ -7,8 +7,8 @@ declare http_enabled=true
 if bashio::config.true 'ssl'; then
   bashio::config.require.ssl
   http_enabled=false
-  sed -i '4 i https-certificate-file /ssl/{{ .certfile }};' /var/app/conf/keycloak.gtpl
-  sed -i '4 i https-certificate-key-file /ssl/{{ .keyfile }};' /var/app/conf/keycloak.gtpl
+  sed -i '4 i https-certificate-file /ssl/{{ .certfile }}' /var/app/conf/keycloak.gtpl
+  sed -i '4 i https-certificate-key-file /ssl/{{ .keyfile }}' /var/app/conf/keycloak.gtpl
   bashio::log.info "Ssl enabled, please use https for connection"
 else
   sed -i "4 i proxy-headers=xforwarded" /var/app/conf/keycloak.gtpl
